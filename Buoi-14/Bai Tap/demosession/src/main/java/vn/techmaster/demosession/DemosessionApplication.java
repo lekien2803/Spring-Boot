7404eb42fe4afbd3c17cfe05917f67d4ee36f56d
Kiên <fukz2803@gmail.com>
2022-06-21 15:42:19 +0700

    a

---
+++ Buoi-14/Bai Tap/demosession/src/main/java/vn/techmaster/demosession/DemosessionApplication.java
@@ -0,0 +1,13 @@
+package vn.techmaster.demosession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosessionApplication.class, args);
+	}
+
+}
---
+++ Buoi-14/Bai Tap/demosession/src/test/java/vn/techmaster/demosession/DemosessionApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.techmaster.demosession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
