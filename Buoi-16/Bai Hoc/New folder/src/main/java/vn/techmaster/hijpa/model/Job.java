a205071dd79aefd490327fa94d306b57888e927b
Kiên <fukz2803@gmail.com>
2022-06-25 18:47:19 +0700

    a

--- Buoi-16/Bai Hoc/New folder/src/main/java/vn/techmaster/hijpa/model/Job.java
+++ Buoi-16/Bai Hoc/New folder/src/main/java/vn/techmaster/hijpa/model/Job.java
@@ -4,18 +4,25 @@ import java.util.UUID;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @Table
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Job {
-  @Id @GeneratedValue private UUID id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
   private String title;
   private String description;
 }
--- Buoi-16/Bai Hoc/New folder/src/main/resources/job.sql
+++ Buoi-16/Bai Hoc/New folder/src/main/resources/job.sql
@@ -1,9 +1,2 @@
-insert into
-    job (title, description)
-values
-    ('AWS Developer', 'Can code Java in AWS');
-
-insert into
-    job (title, description)
-values
-    ('asd;alsmd Developer', 'Can code Java in AWS');
+insert into job (title, description)  values ('1', 'Can code Java in AWS');
+insert into  job (title, description) values ('1', 'Can code Java in AWS');
