7d1f1ac124dd5b03ca093d225f35a02802135741
Kien <fukz2803@gmail.com>
2022-07-05 19:11:43 +0700

    update

--- Buoi-18/Bai Tap/blog/src/main/java/vn/techmaster/blog/repository/UserRepository.java
+++ Buoi-18/Bai Tap/blog/src/main/java/vn/techmaster/blog/repository/UserRepository.java
@@ -2,14 +2,18 @@ package vn.techmaster.blog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import vn.techmaster.blog.dto.UserDto;
 import vn.techmaster.blog.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    @Query("select new vn.techmaster.blog.dto.UserDto(u.id, u.name, u.email, u.avatar) from User u where u.id = ?1")
-    UserDto getUserDto(Integer id);
+    @Query("select new vn.techmaster.blog.dto.UserDto(u.id, u.name, u.email, u.avatar) from User u where u.id = :id")
+    UserDto getUserDto(@Param("id") Integer id);
+
+//    @Query("select new vn.techmaster.blog.dto.UserDto(u.id, u.name, u.email, u.avatar) from User u where u.id = ?1")
+//    UserDto getUserDto(Integer id);
 
     <T> T getUserById(Integer id, Class<T> type);
 }
